d13adcbbdb66239b526b9e06a275800a81c43db3
snail9611 <1289732630@qq.com>
2025-06-21 19:27:48 +0800

    create others.md

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'experience', 'publications', 'awards'];
+const section_names = ['home', 'experience', 'publications', 'awards', 'others'];
 
 
 window.addEventListener('DOMContentLoaded', event => {
